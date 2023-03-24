3bca8d737590c8bf9c78679a3373a570ff63f74d
Shane Stuteville <shane.stuteville@gmail.com>
2023-03-24 09:42:02 -0500

    cli checkbook ledger project

--- checkbook.py
+++ checkbook.py
@@ -4,62 +4,67 @@ the user with '\n~~~ Welcome to your command line checkbook ~~~\n'. The program 
 be used to store the float current_balance. If the variable current_balance doesn't exist, create it. The following will be displayed to the users as their choices:
 '1) view current_balance', '2) record a debt (withdrawl)', '3) record a credit(deposit)', '4) exit'. If the user enters 1, then the program displays the
 current_balance from the ledger.txt file. If the user enters 2, then the program will prompt the user with 'How much is the debit?\n\n' and the entered float will 
-be subtracted from the current_balance and saved as the new current_balance. If the user enters 3, then the program will prompt the user with 'How much is the 
-credit\n\n? and the entered float will be added to the current-balance and saved as the new current_balance. If the user enter 4, then the program will display 
-'Thanks, have a great day!\n' and exit back to the terminal prompt. If the user enters any other response, then display 'That response is not allowed'. 
-The program needs to continue to loop until the user selects 4 to exit.
+be checked to make sure it is a valid float and then subtracted from the current_balance and saved as the new current_balance. If the user enters 3, then the program 
+will prompt the user with 'How much is the credit\n\n? and the entered float will be checked to make sure it is a valid float and then added to the current-balance 
+and saved as the new current_balance. If the user enter 4, then the program will display 'Thanks, have a great day!\n' and exit back to the terminal prompt. If the 
+user enters any other response, then display 'That response is not allowed'. The program needs to continue to loop until the user selects 4 to exit.
 
 '''
 
-# Code to create a checkbook program
+# checkbook program
 
-# Import necessary libraries
-import os
-
-# Welcome the user
+# print the welcome message
 print("\n~~~ Welcome to your command line checkbook ~~~\n")
 
-# Check if the ledger.txt file exists
-if os.path.exists("ledger.txt"):
-    # Read the current_balance from the ledger.txt file
+# initialize the balance to 0 if it doesn't exist in ledger.txt
+try:
     with open("ledger.txt", "r") as ledger_file:
         current_balance = float(ledger_file.read().strip())
-else:
-    # Create the ledger.txt file and initialize the current_balance as 0
+except FileNotFoundError:
     with open("ledger.txt", "w") as ledger_file:
-        current_balance = 0
-        ledger_file.write(str(current_balance))
+        ledger_file.write("0")
+    current_balance = 0
 
-# Continuously prompt the user for a choice until they choose to exit
+# loop until user selects option 4 for exit
 while True:
-    print("What would you like to do?")
-    print("1) view current_balance")
-    print("2) record a debt (withdrawl)")
-    print("3) record a credit (deposit)")
-    print("4) exit\n")
-    choice = int(input().strip())
-    
-    # View the current_balance
-    if choice == 1:
-        print("\nYour current_balance is: $%.2f\n" % current_balance)
-    # Record a debt (withdrawl)
-    elif choice == 2:
-        debt = float(input("\nHow much is the debit?\n\n"))
-        current_balance -= debt
-        # Save the new current_balance to the ledger.txt file
-        with open("ledger.txt", "w") as ledger_file:
-            ledger_file.write(str(current_balance))
-    # Record a credit (deposit)
-    elif choice == 3:
-        credit = float(input("\nHow much is the credit?\n\n"))
-        current_balance += credit
-        # Save the new current_balance to the ledger.txt file
-        with open("ledger.txt", "w") as ledger_file:
-            ledger_file.write(str(current_balance))
-    # Exit the program
-    elif choice == 4:
+    # prompt the user for their choice
+    choice = input("What would you like to do?\n"
+                   "1) view current balance\n"
+                   "2) record a debt (withdrawl)\n"
+                   "3) record a credit (deposit)\n"
+                   "4) exit\n\n")
+
+    # view current balance
+    if choice == "1":
+        print("\nCurrent balance:", current_balance, "\n")
+
+    # record a debt (withdrawl)
+    elif choice == "2":
+        debit = input("How much is the debit?\n\n")
+        try:
+            debit = float(debit)
+            current_balance -= debit
+            with open("ledger.txt", "w") as ledger_file:
+                ledger_file.write(str(current_balance))
+        except ValueError:
+            print("Invalid debit amount. Please enter a valid number.\n")
+
+    # record a credit (deposit)
+    elif choice == "3":
+        credit = input("How much is the credit?\n\n")
+        try:
+            credit = float(credit)
+            current_balance += credit
+            with open("ledger.txt", "w") as ledger_file:
+                ledger_file.write(str(current_balance))
+        except ValueError:
+            print("Invalid credit amount. Please enter a valid number.\n")
+
+    # exit the program
+    elif choice == "4":
         print("\nThanks, have a great day!\n")
         break
-    # Invalid choice
+
+    # invalid choice
     else:
-        print("\nInvalid choice, please enter a valid choice\n")
+        print("That response is not allowed.\n")
